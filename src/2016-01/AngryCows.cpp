95140978176ab75bc8950a79babdbd8c0c3d0145
RainbowDragon <xuanxiang@hotmail.com>
2021-11-09 13:41:44 -0800

    USACO 2016 - 01 - Problem 1 - Angry Cows

--- src/2016-01/AngryCows.cpp
+++ src/2016-01/AngryCows.cpp
@@ -24,29 +24,7 @@ int main()
     }
     sort(points.begin(), points.end());
     
-    bool dp[t+1][2];
-    dp[0][0] = true;
-    dp[0][1] = true;
-
     int result = 0;
-    for (int i = 1; i <= t; i++)
-    {
-        dp[i][0] = false;
-        dp[i][1] = false;
-        if ((i >= a && dp[i-a][0]) || (i >= b && dp[i-b][0])) {
-            dp[i][0] = true;
-            dp[i/2][1] = true;
-            result = max(result, i);
-        }
-    }
-
-    for (int i = 1; i <= t; i++)
-    {
-        if ((i >= a && dp[i-a][1]) || (i >= b && dp[i-b][1])) {
-            dp[i][1] = true;
-            result = max(result, i);
-        }
-    }
 
     fout << result << endl;
 
